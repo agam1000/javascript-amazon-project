2a03aba42a85c968d1d4a9c062aacd87292f589a
Agam jain <aagamj1112@gmail.com>
2025-05-31 19:57:15 +0530

    made the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,12 +46,36 @@ products.forEach((product)=>{
             Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary js-button-add-to-cart" data-product-id="${product.id}">
             Add to Cart
         </button>
     </div>
     `;
-    console.log(productHTML);
 });
 
 document.querySelector(".js-products-grid").innerHTML=productHTML;
+document.querySelectorAll(".js-button-add-to-cart").forEach((button)=>{
+    button.addEventListener('click',()=>{
+        const productId=button.dataset.productId;//important
+        let matchingItem;
+        cart.forEach((item)=>{
+            if(productId===item.productId){
+                matchingItem=item;
+            }
+        });
+        if(matchingItem){
+            matchingItem.quantity+=1
+        }else{
+            cart.push({
+                productId:productId,
+                quantity:1
+            });
+        }
+        console.log(cart);
+        let totalQuantity=0;
+        cart.forEach((item)=>{
+            totalQuantity+= item.quantity
+        })
+        document.querySelector(".js-cart-quantity").innerHTML=totalQuantity;
+    });
+});
