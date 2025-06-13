f926a6f64536de1d5457e98aef5c7540088f5514
Agam jain <aagamj1112@gmail.com>
2025-06-13 18:25:58 +0530

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,16 @@
-export let cart=[{
-    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity:2,
-},{
-    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity:1
-}];
+export let cart=JSON.parse(localStorage.getItem('cart'));//returns string version of the cart if JSON.parse is not used 
+if(!cart){
+    cart=[{
+        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity:2,
+    },{
+        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity:1
+    }];   
+}
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));//saved as a string
+}
 export function addToCart(productId,button){
     
     let matchingItem;
@@ -33,4 +39,5 @@ export function removeFromCart(productId){
         }
     });
     cart = newCart;
+    saveToStorage();
 }
