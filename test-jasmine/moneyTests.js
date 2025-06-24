4f5c50c5e438a555c4dc00cd4f8fa991cbbfe297
Agam jain <aagamj1112@gmail.com>
2025-06-24 11:07:06 +0530

    tested formatCurrency using jasmine

---
+++ test-jasmine/moneyTests.js
@@ -0,0 +1,13 @@
+import { formatCurrency } from "../scripts/utilities/money.js";
+
+describe('test suit:formatCurrency',()=>{
+    it('converts cents into dollars',()=>{
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+    it('works with 0',()=>{
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+    it('rounds up to nearest cents',()=>{
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+});
